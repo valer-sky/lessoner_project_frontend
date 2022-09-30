d670ac2a6941bfd12cb5d06f4de08e0a84a88ce7
Igor <tanovigor@gmail.com>
2022-09-30 12:03:21 +0300

    add link

--- src/components/body/header/Header.tsx
+++ src/components/body/header/Header.tsx
@@ -1,7 +1,8 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 
 
@@ -9,17 +10,21 @@ const Header:any = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href='/' >Lessoner</Navbar.Brand>
+
+                <Container>
+                <Link to="/"><Navbar.Brand  >Lessoner</Navbar.Brand></Link>
+                
                 <Nav className="me-auto">  
-                    <Nav.Link href='/categories' className='nav-link'>Categories</Nav.Link>
-                    <Nav.Link href='/lessons' className='nav-link' >Lessons</Nav.Link>
-                    <Nav.Link href='/about' className='nav-link'>About</Nav.Link>
+                    <Link to="/categories" className='nav-link'>Categories</Link>
+                    <Link to="/lessons"className='nav-link'>Lessons</Link>
+                    <Link to="/about" className='nav-link'>About</Link>
                 </Nav>
                 <Nav className="me-auto">
                     <Nav.Link >English</Nav.Link>
                     <Nav.Link >Русский</Nav.Link>  
                 </Nav>
-                <Button href="/users/sign_in" variant="dark" className="btn btn-outline-light btn-sm">Log in</Button>
+                <Link to="/users/sign_in"><Button  variant="dark" className="btn btn-outline-light btn-sm">Log in</Button></Link>
+                </Container>
             </Navbar>
         </header>
     )
