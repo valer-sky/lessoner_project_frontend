244de940ad5c555b7aed1ebc770a8388b451b650
Pavel <kurilo.pavel@mail.ru>
2022-10-04 16:23:48 +0300

    fix: fixed mistake

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -3,12 +3,12 @@ import open_eye from "./icons/open_eye.svg";
 import close_eye from "./icons/close_eye.svg";
 import "./password.scss";
 
-type PasseordProps = {
+type PasswordProps = {
   minSymbol: number;
   maxSymbol: number;
 }
 
-const Password = ({minSymbol, maxSymbol}: PasseordProps) => {
+const Password = ({minSymbol, maxSymbol}: PasswordProps) => {
   const passwordRegex = new RegExp("^[-/\\=!#$%&'*+?^_`{|}~.A-Z\d]{" + minSymbol + "," + maxSymbol + "}$", "i");
   const [value, setValue] = useState('');
   const [isError, setIsError] = useState(false);
@@ -16,7 +16,6 @@ const Password = ({minSymbol, maxSymbol}: PasseordProps) => {
   const [visiblePassword, setVisiblePassword] = useState(false);
 
   const fieldHandler = (e: React.FormEvent<HTMLInputElement>) => {
-    console.log(isError)
     if (passwordRegex.test(e.currentTarget.value)) {
       setIsError(false);
     } else {
@@ -26,9 +25,7 @@ const Password = ({minSymbol, maxSymbol}: PasseordProps) => {
   }
 
   const blurHandle = (e: React.FormEvent<HTMLInputElement>) => {
-    if (e.currentTarget.type === 'password') {
-      setIsBlur(true);
-    }
+    setIsBlur(true);
   }
   const showPassword = (e: React.MouseEvent<HTMLImageElement>): void => {
     if (visiblePassword !== false) {
@@ -42,9 +39,10 @@ const Password = ({minSymbol, maxSymbol}: PasseordProps) => {
     <div className={'password'}>
       <label className={'passwordLabel'}>Password</label>
       <input type={visiblePassword ? 'text' : 'password'}
-             className={'passwordInput ' + `${isError ? 'errorInput' : ''}`}
+             className={'passwordInput ' + `${(isError && isBlur) ? 'errorInput' : ''}`}
              onChange={fieldHandler}
-             onBlur={blurHandle}/>
+             onBlur={blurHandle}
+             required/>
       <img className={'image'} alt='eye' src={visiblePassword ? open_eye : close_eye} onClick={showPassword}/>
       {(isError && isBlur) && <span className={'error'}>Password must contain from 6 to 256 characters</span>}
     </div>
