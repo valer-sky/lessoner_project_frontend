3348838f675b61ac72a22da7b858176fdb5c15d9
Pavel <kurilo.pavel@mail.ru>
2022-10-05 14:45:13 +0300

    feat: completed task 40 Birthday

---
+++ src/components/Birthday.tsx
@@ -0,0 +1,29 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {useState} from "react";
+import "./birthday.scss";
+
+const Birthday = () => {
+  const [value, setValue] = useState<any>(null)
+
+  const fieldHandler = (event: Date) => {
+    setValue(event)
+  }
+
+  return (
+    <div className={'birthday'}>
+      <label className={'birthdaylabel'}>When is your birthday?</label>
+      <DatePicker onChange={(e: any) => fieldHandler(e)}
+                  selected={value}
+                  placeholderText={'--.--.----'}
+                  dateFormat={'dd.MM.yyyy'}
+                  maxDate={new Date()}
+                  minDate={new Date('01.01.1900')}
+                  showMonthDropdown
+                  showYearDropdown
+                  dropdownMode="select"
+                  className={'birthdayInput'}/>
+    </div>
+  )
+}
+export default Birthday;
