12e7493a7738b551437f3b600d45b13e2f059b6b
Pavel <kurilo.pavel@mail.ru>
2022-10-05 21:03:16 +0300

    fix: fixed mistake

--- src/components/Birthday.tsx
+++ src/components/Birthday.tsx
@@ -4,25 +4,25 @@ import {useState} from "react";
 import "./birthday.scss";
 
 const Birthday = () => {
-  const [value, setValue] = useState<any>(null)
-
+  const [value, setValue] = useState<any>(null);
   const fieldHandler = (event: Date) => {
-    setValue(event)
+    setValue(event);
   }
 
   return (
-    <div className={'birthday'}>
-      <label className={'birthdaylabel'}>When is your birthday?</label>
+    <div className='birthday'>
+      <label className='birthdaylabel'>When is your birthday?</label>
       <DatePicker onChange={(e: any) => fieldHandler(e)}
                   selected={value}
-                  placeholderText={'--.--.----'}
-                  dateFormat={'dd.MM.yyyy'}
+                  placeholderText='--.--.----'
+                  dateFormat='dd.MM.yyyy'
                   maxDate={new Date()}
                   minDate={new Date('01.01.1900')}
                   showMonthDropdown
                   showYearDropdown
                   dropdownMode="select"
-                  className={'birthdayInput'}/>
+                  className='birthdayInput'
+                  required/>
     </div>
   )
 }
