4bd17816488b1989da95cee93a2727f35e81aefb
Nalallia <mnatasha11@mail.ru>
2022-10-16 23:45:20 +0300

    added registration form component

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,52 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import './Email.scss';
 
 type EmailProps = {
-	value: string;
-	error: string;
-	handleChanges: Function;
-	handleBlur: Function;
+	field: object;
+	error?: string;
 }
 
-
-const Email = ({ value, error, handleChanges, handleBlur }: EmailProps) => {
-	// const [value, setValue] = useState('');
-	// const [error, setError] = useState('');
-
-	// const handleChanges = (e: React.FormEvent<HTMLInputElement>): void => {
-		// e.preventDefault();
-		// setValue(e.currentTarget.value);
-	// };
-
-	const emailValidation = () => {
-		let message: string;
-		const invalidationRules = [
-			/^\s*$/, // check string not empty
-			/^[^@]+$/, // @ should exist
-			/@[^@]*@/, // onle one @ is admissible
-			/^\./, // '.' can't be first symbol
-			/\.{2,}.+(?=@)/, // '.' can't repeat more than once in a row
-			/\.(?=@)/, // '.' can't be before @
-			/[^A-Za-z0-9_!#$%&'.*+\-/=?\^`{|}~].*(?=@)/, // include only valid symbols before @
-			/(?<=@).*[^a-z0-9\-.]/, // include only valid symbols before @
-		];
-
-		// if (invalidationRules.some(rule => rule.test(value))) {
-		// 	message = 'Please enter a valid email address';
-		// 	cbGetErrors({ email: message });
-		// } else if (!isValid) {
-		// 	message = 'This email address is already registered.';
-		// 	cbGetErrors({ email: message });
-		// } else {
-		// 	message = '';
-		// }
-		// setError(message);
-	};
+const Email = ({ field, error }: EmailProps): JSX.Element => {
+	const [value, setValue] = useState('');
 
 	return (
 		<div className="Email-Wrapper">
 			<label htmlFor="email" className="Email-Label">Email</label>
-			<input type="text" name="email" required minLength={3} maxLength={256} className={"Email-Input" + (error ? " invalid-Email-Input" : "")} placeholder="username@gmail.com" value={value} /* onChange={handleChanges} onBlur={handleBlur} */ />
+			<input type="text" name="email" required minLength={3} maxLength={256} className={"Email-Input" + (error ? " invalid-Email-Input" : "")} placeholder="username@gmail.com" value={value} onChange={(e) => setValue(e.currentTarget.value)} {...field} />
 			{error && <div><span className='error-Message'>{error}</span></div>}
 		</div>
 	)
--- src/components/FirstRegistrationForm.tsx
+++ src/components/FirstRegistrationForm.tsx
@@ -1,117 +1,90 @@
-import { Formik, Form } from 'formik';
-import './FirstRegistrationForm.scss';
+import { useState } from "react";
+import { Formik, Form, Field } from 'formik';
 import Email from "./Email";
-
-type RegistrationFormObjectType = {
-	email: string,
-	password: string,
-	confirmPassword: string,
-	agreeCheckbox: boolean
-}
+import PasswordAndConfirm from './PasswordAndConfirm';
+import Checkbox from './Checkbox';
+import './FirstRegistrationForm.scss';
 
 interface FormValues {
 	email: string;
 	password: string;
 	confirmPassword: string;
-	agreeCheckbox: boolean;
+	termsAndConditions: boolean;
 }
 
-const FirstRegistrationForm = () => {
-	const emailInvnvalidationRules = [
-		/^\s*$/, // check string not empty
-		/^[^@]+$/, // @ should exist
-		/@[^@]*@/, // onle one @ is admissible
-		/^\./, // '.' can't be first symbol
-		/\.{2,}.+(?=@)/, // '.' can't repeat more than once in a row
-		/\.(?=@)/, // '.' can't be before @
-		/[^A-Za-z0-9_!#$%&'.*+\-/=?\^`{|}~].*(?=@)/, // include only valid symbols before @
-		/(?<=@).*[^a-z0-9\-.]/, // include only valid symbols before @
-	];
-	const passwordLengthLimits = [6, 256];
-	const passwordRegex = new RegExp("^[-/=!#$%&'*+?^_`{|}~.A-Z0-9]{" + passwordLengthLimits[0] + "," + passwordLengthLimits[1] + "}$", "i");
+interface FormErrors {
+	[key: string]: any
+}
 
-	const formValidation = (values: RegistrationFormObjectType) => {
-		const errors: RegistrationFormObjectType = { email: '', password: '', confirmPassword: '', agreeCheckbox: false };
-		if (emailInvnvalidationRules.some(rule => rule.test(values.email))) {
+const emailInvalidationRules = [
+	/^\s*$/, // check string not empty
+	/^[^@]+$/, // @ should exist
+	/@[^@]*@/, // onle one @ is admissible
+	/^\./, // '.' can't be first symbol
+	/\.{2,}.+(?=@)/, // '.' can't repeat more than once in a row
+	/\.(?=@)/, // '.' can't be before @
+	/[^A-Za-z0-9_!#$%&'.*+\-/=?\^`{|}~].*(?=@)/, // include only valid symbols before @
+	/(?<=@).*[^a-z0-9\-.]/, // include only valid symbols before @
+];
+
+const minSymbol = 6;
+const maxSymbol = 256;
+const passwordRegex = new RegExp("^[-/=!#$%&'*+?^_`{|}~.A-Z0-9]{" + minSymbol + "," + maxSymbol + "}$", "i");
+
+const FirstRegistrationForm = () => {
+	const initialValues: FormValues = {
+		email: '',
+		password: '',
+		confirmPassword: '',
+		termsAndConditions: false
+	}
+
+	const validate = (values: FormValues) => {
+		let errors: FormErrors = {};
+		if (emailInvalidationRules.some(rule => rule.test(values.email))) {
 			errors.email = 'Please enter a valid email address';
-		} else if (!passwordRegex.test(values.password)) {
-			errors.password = "An invalid character is present in the password. Password must be between 6 and 256 characters. upper or lower case Latin letters (a–z, A–Z);numbers from 0 to 9;symbols ! # $ % & ' * + - / = ? ^ _ ` { | } ~";
-		} else if (values.password !== values.confirmPassword) {
-			errors.password = 'Passwords do not match';
-		} else if (!values.agreeCheckbox) {
-			errors.password = 'You must consent to the processing of your personal data, in accordance with the Terms';
+		}
+		if (!passwordRegex.test(values.password)) {
+			errors.password = `An invalid character is present in the password. Password must be between ${minSymbol} and ${maxSymbol} characters; upper or lower case Latin letters (a–z, A–Z);numbers from 0 to 9;symbols ! # $ % & ' * + - / = ? ^ _ \` { | } ~`;
+		}
+		if (values.password.length >= maxSymbol || values.password.length < minSymbol) {
+			errors.password = `Password must be between ${minSymbol} and ${maxSymbol} characters`;
+		}
+		if (values.password !== values.confirmPassword) {
+			errors.confirmPassword = 'Passwords do not match';
+		}
+		if (!(values.termsAndConditions)) {
+			errors.termsAndConditions = 'You must consent to the processing of your personal data, in accordance with the Terms';
 		}
 		return errors;
 	}
-	const submitFirstStepForm = (values: RegistrationFormObjectType, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
-		setTimeout(() => {
-			alert(JSON.stringify(values, null, 2));
-			setSubmitting(false);
-		}, 400);
-	}
+
+	const submitFirstStepForm = (values: FormValues) => {
+		console.log(values);
+	};
+
 	return (
-		<Formik
-			initialValues={{ email: '', password: '', confirmPassword: '', agreeCheckbox: false }}
-			validate={formValidation}
-			onSubmit={submitFirstStepForm}
-		>
-			{({
-				values,
-				errors,
-				touched,
-				handleChange,
-				handleBlur,
-				handleSubmit,
-				isSubmitting,
-				/* and other goodies */
-			}) => (
-				<form className="Form__submit">
-					<Email value={values.email} error={errors.email!} handleChanges={handleChange} handleBlur={handleBlur} />
-					<input
-						type="email"
-						name="email"
-						onChange={handleChange}
-						onBlur={handleBlur}
-						value={values.email}
-					/>
-					{errors.email && touched.email && errors.email}
-					<input
-						type="password"
-						name="password"
-						onChange={handleChange}
-						onBlur={handleBlur}
-						value={values.password}
-					/>
-					{errors.password && touched.password && errors.password}
-					<button type="submit" disabled={isSubmitting}>
-						Submit
-					</button>
-				</form>
-			)}
-		</Formik>
+		<div>
+			<Formik
+				initialValues={initialValues}
+				validateOnChange={false}
+				validate={validate}
+				onSubmit={submitFirstStepForm}
+			>
+				{({ errors, touched }) => {
+					return (
+						<Form className="First-Registration-Form">
+							<Field name='email' component={Email} error={touched.email ? errors.email : undefined} />
+							<Field name='password' component={PasswordAndConfirm} minSymbol={minSymbol} maxSymbol={maxSymbol} isConfirm={false} error={touched.password ? errors.password : undefined} />
+							<Field name='confirmPassword' component={PasswordAndConfirm} minSymbol={minSymbol} maxSymbol={maxSymbol} isConfirm={true} error={touched.confirmPassword ? errors.confirmPassword : undefined} />
+							<Field name='termsAndConditions' component={Checkbox} error={touched.termsAndConditions ? errors.termsAndConditions : undefined} />
+							<button type="submit">Next</button>
+						</Form>
+					)
+				}}
+			</Formik>
+		</div>
 	)
-
-
-
-	// return (
-	//     <div>
-	//         <Formik
-	//             initialValues={{ email: '', password: '' }}
-	//             onSubmit={submit}
-	//         >
-	//             {({ setValues, setErrors, values, errors }) => (
-	//                 <Form className="Form__submit">
-	//                     <Email isValid={true}
-	//                         cbGetEmail={(emailVal: { email: string }) => setValues({ ...values, ...emailVal })}
-	//                         cbGetErrors={(emailErr: { error: string }) => setErrors({ ...errors, ...emailErr })} />
-	//                     <div><button type="submit" >
-	//                         Next
-	//                     </button></div>
-	//                 </Form>
-	//             )}
-	//         </Formik>
-	//     </div>
-	// )
 };
 
 export default FirstRegistrationForm;
