e52a2c33bd5326de13004c0191025049ac205e36
Nalallia <mnatasha11@mail.ru>
2022-10-17 00:11:57 +0300

    Revert "added registration form component"
    This reverts commit 4bd17816488b1989da95cee93a2727f35e81aefb.

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,27 +1,27 @@
+import React, {useState} from 'react';
 import classes from './Checkbox.module.scss';
+import '../index.scss';
 
-type CheckboxProps = {
-	field: object;
-	error?: string;
-}
-const Checkbox = ({ field, error }: CheckboxProps): JSX.Element => {
-	return (
-		<div className={classes.wrapper}>
-			<label className={classes.label}>
-				<div>
-					<input
-						className={classes.input}
-						type='checkbox'
-						{...field}
-					/>
-					<p>
-						I agree to the processing of my personal data in accordance with the <a href='#!'>Terms</a>
-					</p>
-				</div>
-				{error && <div><span className='error-Message'>{error}</span></div>}
-			</label>
-		</div>
-	);
+const Checkbox: React.FC = (props) => {
+    const [isChecked, setIsChecked] = useState(false);
+    return (
+        <div className={classes.wrapper}>
+            <label className={classes.label}>
+                <input 
+                    className={classes.input}
+                    type='checkbox'
+                    name='Agree'
+                    checked={isChecked}
+                    onChange={e => setIsChecked(e.target.checked)} 
+                    {...props}
+                />
+                <p>
+                    I agree to the processing of my personal data in accordance with the <a href='#!'>Terms</a> 
+                </p>
+            </label>
+        </div>
+    );
 }
 
 export default Checkbox;
+ 
--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,18 +1,52 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import './Email.scss';
 
 type EmailProps = {
-	field: object;
-	error?: string;
+	value: string;
+	error: string;
+	handleChanges: Function;
+	handleBlur: Function;
 }
 
-const Email = ({ field, error }: EmailProps): JSX.Element => {
-	const [value, setValue] = useState('');
+
+const Email = ({ value, error, handleChanges, handleBlur }: EmailProps) => {
+	// const [value, setValue] = useState('');
+	// const [error, setError] = useState('');
+
+	// const handleChanges = (e: React.FormEvent<HTMLInputElement>): void => {
+		// e.preventDefault();
+		// setValue(e.currentTarget.value);
+	// };
+
+	const emailValidation = () => {
+		let message: string;
+		const invalidationRules = [
+			/^\s*$/, // check string not empty
+			/^[^@]+$/, // @ should exist
+			/@[^@]*@/, // onle one @ is admissible
+			/^\./, // '.' can't be first symbol
+			/\.{2,}.+(?=@)/, // '.' can't repeat more than once in a row
+			/\.(?=@)/, // '.' can't be before @
+			/[^A-Za-z0-9_!#$%&'.*+\-/=?\^`{|}~].*(?=@)/, // include only valid symbols before @
+			/(?<=@).*[^a-z0-9\-.]/, // include only valid symbols before @
+		];
+
+		// if (invalidationRules.some(rule => rule.test(value))) {
+		// 	message = 'Please enter a valid email address';
+		// 	cbGetErrors({ email: message });
+		// } else if (!isValid) {
+		// 	message = 'This email address is already registered.';
+		// 	cbGetErrors({ email: message });
+		// } else {
+		// 	message = '';
+		// }
+		// setError(message);
+	};
 
 	return (
 		<div className="Email-Wrapper">
 			<label htmlFor="email" className="Email-Label">Email</label>
-			<input type="text" name="email" required minLength={3} maxLength={256} className={"Email-Input" + (error ? " invalid-Email-Input" : "")} placeholder="username@gmail.com" value={value} onChange={(e) => setValue(e.currentTarget.value)} {...field} />
+			<input type="text" name="email" required minLength={3} maxLength={256} className={"Email-Input" + (error ? " invalid-Email-Input" : "")} placeholder="username@gmail.com" value={value} /* onChange={handleChanges} onBlur={handleBlur} */ />
 			{error && <div><span className='error-Message'>{error}</span></div>}
 		</div>
 	)
