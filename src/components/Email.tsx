44d1a93bf14f2ed51a0b275834d0902f3f395af1
Nalallia <mnatasha11@mail.ru>
2022-10-17 23:06:16 +0300

    some improvements in interaction between email and BE

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,44 +1,45 @@
 import React, { useState } from "react";
-import { isEmailExists } from "../services/services";
+import { isEmailExists } from "../services/api/isEmailExists";
 import './Email.scss';
 
 const Email = () => {
-    const [value, setValue] = useState('');
-    const [error, setError] = useState('');
+	const [value, setValue] = useState('');
+	const [error, setError] = useState('');
 
-    const handleChanges = (e: React.FormEvent<HTMLInputElement>): void => {
-        e.preventDefault();
-        setValue(e.currentTarget.value);
-    };
+	const handleChanges = (e: React.FormEvent<HTMLInputElement>): void => {
+		e.preventDefault();
+		setValue(e.currentTarget.value);
+	};
 
-    const emailValidation = async () => {
-        const invalidationRules = [
-            /^\s*$/, // check string not empty
-            /^[^@]+$/, // @ should exist
-            /@[^@]*@/, // onle one @ is admissible
-            /^\./, // '.' can't be first symbol
-            /\.{2,}.+(?=@)/, // '.' can't repeat more than once in a row
-            /\.(?=@)/, // '.' can't be before @
-            /[^A-Za-z0-9_!#$%&'.*+\-/=?\^`{|}~].*(?=@)/, // include only valid symbols before @
-            /(?<=@).*[^a-z0-9\-.]/, // include only valid symbols before @
-        ];
+	const emailValidation = async () => {
+		const isExists = await isEmailExists(value);
+		const invalidationRules = [
+			/^\s*$/, // check string not empty
+			/^[^@]+$/, // @ should exist
+			/@[^@]*@/, // only one @ is admissible
+			/^\./, // '.' can't be first symbol
+			/\.{2,}.+(?=@)/, // '.' can't repeat more than once in a row
+			/\.(?=@)/, // '.' can't be before @
+			/[^A-Za-z0-9_!#$%&'.*+\-/=?\^`{|}~].*(?=@)/, // include only valid symbols before @
+			/(?<=@).*[^a-z0-9\-.]/, // include only valid symbols before @
+		];
 
-        if (invalidationRules.some(rule => rule.test(value))) {
-            setError('Please enter a valid email address');
-        } else if (await isEmailExists(value)) {
-            setError('This email address is already registered');
-        } else {
-            setError('');
-        }
-    };
+		if (invalidationRules.some(rule => rule.test(value))) {
+			setError('Please enter a valid email address');
+		} else if (isExists) {
+			setError('This email address is already registered');
+		} else {
+			setError('');
+		}
+	};
 
-    return (
-        <div className="Email-Wrapper">
-            <label htmlFor="email" className="Email-Label">Email</label>
-            <input type="text" name="email" required minLength={3} maxLength={256} className={"Email-Input" + (error ? " invalid-Email-Input" : "")} placeholder="username@gmail.com" value={value} onChange={handleChanges} onBlur={emailValidation} />
-            {error && <div><span className='error-Message'>{error}</span></div>}
-        </div>
-    )
+	return (
+		<div className="Email-Wrapper">
+			<label htmlFor="email" className="Email-Label">Email</label>
+			<input type="text" name="email" required minLength={3} maxLength={256} className={"Email-Input" + (error ? " invalid-Email-Input" : "")} placeholder="username@gmail.com" value={value} onChange={handleChanges} onBlur={emailValidation} />
+			{error && <div><span className='error-Message'>{error}</span></div>}
+		</div>
+	)
 };
 
 export default Email;
