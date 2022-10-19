0ad8d7b1ec728018a4077840ed2ca0d952904039
OxygenO22 <bamaik@mail.ru>
2022-10-19 20:33:42 +0300

    Final commit: with modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ModalConfirmCode from './components/ModalConfirmCode';
+import './components/ModalConfirmCode'
 
 function App() {
+  const [modalActive, setModalActive] = useState<boolean>(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button 
+        className='open-btn' 
+        onClick={() => setModalActive(true)}
+      >
+        Open modal
+      </button>
+      <ModalConfirmCode active={modalActive} setActive={setModalActive}/>
     </div>
   );
 }
