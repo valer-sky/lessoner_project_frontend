4ea871030ecf16764f867349d0e4e3ed2afe830e
Igor <tanovigor@gmail.com>
2022-10-19 18:49:00 +0300

    Lints err

--- src/components/body/content/Main.tsx
+++ src/components/body/content/Main.tsx
@@ -3,8 +3,9 @@ import Lessoner from "./lessoner/Lessoner";
 import Categories from "./categories/Categories";
 import Lessons from "./lessons/Lessons";
 import About from "./about/About";
-import Sign_in from "./Sign_in/Sign_in";
-import Sign_up from "./lessoner/Sign_up/Sign_up";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./lessoner/SignUp/SignUp";
+
 
 const Content = () => {
   return (
@@ -14,8 +15,8 @@ const Content = () => {
         <Route path="/categories" element={<Categories />} />
         <Route path="/lessons" element={<Lessons />} />
         <Route path="/about" element={<About />} />
-        <Route path="/users/sign_in" element={<Sign_in />} />
-        <Route path="/users/sign_up" element={<Sign_up />} />
+        <Route path="/users/sign_in" element={<SignIn />} />
+        <Route path="/users/sign_up" element={<SignUp />} />
       </Routes>
     </div>
   );
