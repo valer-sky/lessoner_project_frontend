86cf7806bfcc56dd8a6f7f66786857e8bfd1ec29
Kurilo-Pavel <75971336+Kurilo-Pavel@users.noreply.github.com>
2022-10-21 23:06:49 +0300

    Update userSlice.ts

--- src/store/loginName/userSlice.ts
+++ src/store/loginName/userSlice.ts
@@ -1,4 +1,4 @@
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {BACKEND_URL} from "../../constants";
 
 export const getUser = createAsyncThunk(
