d9dff9d3a093bababa72668301b1c1facb12507d
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-10-26 20:29:12 +0300

    addd basic internalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/body/Body";
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/LoginPage';
 import FirstRegistrationForm from "./components/FirstRegistrationForm";
+import { useState } from 'react';
+import { IntlProvider } from 'react-intl'; 
+import TranslationHelpers from './components/translations/translationHelpers';
 
-function App(): any {
+function App(): JSX.Element {
+  const [languageCode, setLanguageCode] = useState(TranslationHelpers.getCurrentLanguageCode());
+  const messages = TranslationHelpers.getLanguageMessages(languageCode);
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Body />
-        <Routes>
-        <Route path='/users/sign_in' element={<LoginPage/>}/>
-        <Route path='/users/sign_up' element={<FirstRegistrationForm/>}/>
-      </Routes>
-      </div>
-    </BrowserRouter>
+    <IntlProvider locale={languageCode} messages={messages}>
+      <BrowserRouter>
+        <div className="App">
+          <Body onLanguageSwitch={setLanguageCode} />
+          <Routes>
+            <Route path='/users/sign_in' element={<LoginPage/>} />
+            <Route path='/users/sign_up' element={<FirstRegistrationForm/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </IntlProvider>
   );
 }
 
