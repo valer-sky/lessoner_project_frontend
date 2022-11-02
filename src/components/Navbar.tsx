b5a78fb1973672907c4c6c0dce1ca2a686fdec5f
OxygenO22 <bamaik@mail.ru>
2022-11-02 17:17:33 +0300

    intermediate version 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,37 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import './Navbar.scss';
+import home from "./icons/home.svg";
+import categories from "./icons/categories.svg";
+import subscription from "./icons/subscription.svg";
+import hat_mylesson from "./icons/hat_mylesson.svg";
+import watch_later from "./icons/watch_later.svg";
 
-interface Navbar {
-    items: any;
-    active: boolean;
-    setActive: (bool: boolean) => void;
-}
 
-const Navbar: FC<Navbar> = ({items, active, setActive}) => {
+const Navbar: FC = () => {
+    const [isMenuActive, setIsMenuActive] = useState(false);
+    const items = [
+    {id: 1, value: 'Home', href: '/home', icon: home},
+    {id: 2, value: 'Categories', href: '/categories', icon: categories},
+    {id: 3, value: 'Subscription', href: '/subscription', icon: subscription},
+    {id: 4, value: 'My lessons', href: '/my_lessons', icon: hat_mylesson},
+    {id: 5, value: 'Watch later', href: '/watch_later', icon: watch_later},
+  ]
   return (
-    <div className={active ? 'menu active' : 'menu'} onClick={() => setActive(false)}>
-        <div className={active ? 'blur active' : 'blur'}/>
-        <div className={active ? 'menu__content active' : 'menu__content'} onClick={e => e.stopPropagation()}>
+    <div className={isMenuActive ? 'menu active' : 'menu'} onClick={() => setIsMenuActive(false)}>
+        <div className={isMenuActive ? 'blur active' : 'blur'}/>
+        <div className={isMenuActive ? 'menu__content active' : 'menu__content'} onClick={e => e.stopPropagation()}>
+            <div className="burger-btn" onClick={() => setIsMenuActive(!isMenuActive)}>
+        <span/>
+      </div>
             <ul>
                 {items.map((item: {
                     id: number; href: string; value: string; icon: string; 
                 }) => 
                     <li className='menu__item' key={item.id}>
-                        <div className={!active ? 'image__wrapper active' : 'image__wrapper'}>
+                        <div className={!isMenuActive ? 'image__wrapper active' : 'image__wrapper'}>
                             <img className='image' alt={item.value} src={item.icon}/>
                         </div>
-                        <a className={active ? 'menu__text active' : 'menu__text'} href={item.href} >{item.value}</a>
+                        <a className={isMenuActive ? 'menu__text active' : 'menu__text'} href={item.href} >{item.value}</a>
                     </li>
                 )}
             </ul>
--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,35 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Main from "./content/Main";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Navbar from "../Navbar";
-import home from "../icons/home.svg";
-import categories from "../icons/categories.svg";
-import subscription from "../icons/subscription.svg";
-import hat_mylesson from "../icons/hat_mylesson.svg";
-import watch_later from "../icons/watch_later.svg";
 
 const Body = (props: any) => {
-  const [isMenuActive, setIsMenuActive] = useState(false);
-  const items = [
-    {id: 1, value: 'Home', href: '/home', icon: home},
-    {id: 2, value: 'Categories', href: '/categories', icon: categories},
-    {id: 3, value: 'Subscription', href: '/subscription', icon: subscription},
-    {id: 4, value: 'My lessons', href: '/my_lessons', icon: hat_mylesson},
-    {id: 5, value: 'Watch later', href: '/watch_later', icon: watch_later},
-  ]
+  
+  
   return (
     <>
-      <div className="burger-btn" onClick={() => setIsMenuActive(!isMenuActive)}>
-        <span/>
-      </div>
+      
       <Header />
       <Main />
       <Footer />
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt dicta quos dignissimos ad, expedita quae recusandae, dolor architecto atque optio autem nemo mollitia natus nobis asperiores laborum! Aspernatur repellendus in, optio voluptas accusantium eum temporibus a odit tempora facere ipsum qui nam inventore tempore dolores minus id repudiandae beatae facilis placeat? Totam cumque minus nemo maiores maxime consequuntur quas provident? Id unde distinctio temporibus aut hic itaque quisquam, veritatis qui ab exercitationem soluta cupiditate. Obcaecati quaerat ullam delectus quae ducimus amet? Ducimus, amet quod, voluptates perferendis illo minus quas doloribus pariatur repudiandae at quis iste est quam, mollitia magni commodi.
       </p>
-      <Navbar active={isMenuActive} setActive={setIsMenuActive} items={items}/>
+      <Navbar/>
     </>
   );
 };
