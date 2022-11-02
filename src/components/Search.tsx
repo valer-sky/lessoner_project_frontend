b2c34bef7dc57a8a74bd58e0481c75d1fc148c19
Pavel <kurilo.pavel@mail.ru>
2022-11-02 20:21:37 +0300

    fix: fixed mistakes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,29 @@
 import {Link} from "react-router-dom";
-import Arrow from "./icons/arrow.png";
+import Arrow from "./icons/Arrow.svg";
+import Cross from "./icons/Cross.svg";
 import "./search.scss";
+import {useState} from "react";
 
 const Search = () => {
+  const [isValue, setIsValue] = useState(false);
+  const [value, setValue] = useState('');
+
+  const fieldHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+    if (value.length > 0) {
+      setIsValue(true);
+    }
+  }
+
   return (
     <div className='search-component'>
       <Link to='/' className='search-arrow'>
         <img src={Arrow} alt='arrow'/>
       </Link>
-      <input type='text' placeholder='Search' className='search-input'/>
+      <input type='text' placeholder='Search' onChange={fieldHandler} className='search-input' value={value}/>
+      <div className='search-cross' onClick={() => setValue('')}>
+        <img src={Cross} alt='cross' className='cross'/>
+      </div>
     </div>
   )
 }
