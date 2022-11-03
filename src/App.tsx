a42abbf2acd3e87ac475707b3eaeb77c73d221ee
DzmitryAs <112815387+DzmitryAs@users.noreply.github.com>
2022-11-03 23:10:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,17 @@ function App(): JSX.Element {
   const [languageCode, setLanguageCode] = useState(TranslationHelpers.getCurrentLanguageCode());
   const messages = TranslationHelpers.getLanguageMessages(languageCode);
   return (
+   <IntlProvider locale={languageCode} messages={messages}>
     <BrowserRouter>
       <div className="App">
-        <Body/>
+                <Body onLanguageSwitch={setLanguageCode} />
         <Routes>
           <Route path='/users/sign_in' element={<PhoneNumberPage/>}/>
           <Route path='/users/sign_up' element={<FirstRegistrationForm/>}/>
         </Routes>
       </div>
     </BrowserRouter>
-    <IntlProvider locale={languageCode} messages={messages}>
-           <Body onLanguageSwitch={setLanguageCode} />
-             </IntlProvider>
+   </IntlProvider>
   );
 }
 export default App;
