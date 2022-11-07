51e2781d16f597a6713766a455ba0257a1221299
OxygenO22 <bamaik@mail.ru>
2022-11-07 16:51:30 +0300

    For approwe

--- src/components/NavbarStudio.tsx
+++ src/components/NavbarStudio.tsx
@@ -12,61 +12,58 @@ const NavbarStudio: FC = () => {
     {id: 4, value: 'Management', href: '#!', icon: 'management'},
   ]
 
-  let image__wrapper_cs1 = !isMenuActive ? 'active' : '';
-  let svg_cs1 = isMenuActive ? 'active' : '';
-  let menu__text_cs1 = isMenuActive ? 'active' : '';
+  let imageWrapper = !isMenuActive ? 'image__wrapper--active' : '';
+  let svg = isMenuActive ? 'svg--active' : '';
+  let menuText = isMenuActive ? 'menu__text--active' : '';
 
   return (
-    <div className='menu' onClick={() => setIsMenuActive(false)}>
-        <div className={isMenuActive ? 'blur active' : 'blur'}/>
-        <div className={isMenuActive ? 'menu__content active' : 'menu__content'} onClick={e => e.stopPropagation()}>
-            <div className="menu__inner">      
-                <ul>
-                    {
-                        !isMenuActive ? 
-                        <div className='menu__item' onClick={() => setIsMenuActive(!isMenuActive)}>
-                            <div className='image__wrapper active'>
-                                <div className='svg svg__menu'>
-                                    <NavbarSVGSelector icon='menu'/>
-                                </div>
-                            </div>
-                        </div> :
-                        <div className='menu__item' onClick={() => setIsMenuActive(!isMenuActive)}>
-                            <div className='image__wrapper active'>
-                                <div className='svg'>
-                                    <NavbarSVGSelector icon='close'/>
-                                </div>
+    <div className= 'menu' onClick={() => setIsMenuActive(false)}>
+        <div className={isMenuActive ? 'blur--active' : 'blur'}/>
+        <div className={isMenuActive ? 'menu__content--active' : 'menu__content'} onClick={e => e.stopPropagation()}>
+            <ul className="menu__inner">
+                {
+                    !isMenuActive ? 
+                    <div className='menu__item' onClick={() => setIsMenuActive(!isMenuActive)}>
+                        <div className='image__wrapper image__wrapper--active'>
+                            <div className='svg svg__menu'>
+                                <NavbarSVGSelector icon='menu'/>
                             </div>
                         </div>
-                    }
-                    {
-                        items.map((item: {
-                            id: number; href: string; value: string; icon: string; 
-                        }) => item.icon !== 'menu' && item.icon !== 'close' ? 
-                            <li className={'menu__item'} key={item.id} onClick={() =>
-                                item.id !== isButtonPressed ? 
-                                setIsButtonPressed(item.id) : 
-                                setIsButtonPressed(isButtonPressed)
-                            }>
-                                <a className='menu__item-inner'  href={item.href}>
-                                    <span className={`image__wrapper ${image__wrapper_cs1} ${item.id === isButtonPressed ? 'selected' : ''}`}>
-                                        <div className={`svg ${svg_cs1} ${item.id === isButtonPressed ? 'selected' : ''}`}>
-                                            <NavbarSVGSelector icon={item.icon}/>
-                                        </div>
-                                    </span>
-                                    <span className={`menu__text ${menu__text_cs1} ${item.id === isButtonPressed ? 'selected' : ''}`}>
-                                        {item.value}
-                                    </span>
-                                </a>
-                            </li>
-                            :
-                            null
-                        )
-                    }
-                </ul>
-            </div>
+                    </div> :
+                    <div className='menu__item' onClick={() => setIsMenuActive(!isMenuActive)}>
+                        <div className='image__wrapper image__wrapper--active'>
+                            <div className='svg'>
+                                <NavbarSVGSelector icon='close'/>
+                            </div>
+                        </div>
+                    </div>
+                }
+                {
+                    items.map((item: {
+                        id: number; href: string; value: string; icon: string; 
+                    }) => item.icon !== 'menu' && item.icon !== 'close' ? 
+                        <li className={'menu__item'} key={item.id} onClick={() =>
+                            item.id !== isButtonPressed ? 
+                            setIsButtonPressed(item.id) : 
+                            setIsButtonPressed(isButtonPressed)
+                        }>
+                            <a className='menu__item-inner'  href={item.href}>
+                                <span className={`image__wrapper ${imageWrapper} ${item.id === isButtonPressed ? 'image__wrapper--selected' : ''}`}>
+                                    <div className={`svg ${svg} ${item.id === isButtonPressed ? 'svg--selected' : ''}`}>
+                                        <NavbarSVGSelector icon={item.icon}/>
+                                    </div>
+                                </span>
+                                <span className={`menu__text ${menuText} ${item.id === isButtonPressed ? 'menu__text--selected' : ''}`}>
+                                    {item.value}
+                                </span>
+                            </a>
+                        </li>
+                        :
+                        null
+                    )
+                }
+            </ul>
         </div>
-        
     </div>
   )
 }
