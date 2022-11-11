e0d0b5f0c9e639f315ae1ff4704fd6b17f051674
Igor <tanovigor@gmail.com>
2022-11-11 15:37:01 +0300

    fix errors

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,7 @@
 const BACKEND_URL = "https://lessoner.herokuapp.com";
-const EMAIL = {minLength: 3,
-  maxLength: 256,};
-const PASSWORD = {minLength: 6,
-  maxLength: 256,};
-const USERNAME = {min: 3,
-  max: 256,};
+const EMAIL = { minLength: 3,maxLength: 256 };
+const PASSWORD = { minLength: 6,maxLength: 256 };
+const USERNAME = { min: 3, max: 256 };
 export {
   BACKEND_URL, EMAIL, PASSWORD, USERNAME
 };
